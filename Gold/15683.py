167e0ab1254a510b1ea6955fcbb5c0ba12df01a9
sangbumlikeagod <oironese@naver.com>
2023-09-02 22:02:55 +0900

    9월 2일 제출

--- Gold/15683.py
+++ Gold/15683.py
@@ -1,6 +1,4 @@
-import sys
-sys.stdin = open(__file__.split('\\')[-1][:-3] + '.txt','r')
-sys.setrecursionlimit(100000)
+
 import sys
 sys.stdin = open(__file__.split('\\')[-1][:-3] + '.txt','r')
 
@@ -16,10 +14,67 @@ for i in range(N):
                 camera.append((i, j))
             else: mamamax -= 1
     room += [local]
-print(room, camera)
-# 크기와 
+# print(*room, camera, sep='\n')
+# 카메라의 4방향 좌표들 확인.
+if not camera:
+    print(mamamax)
+else:
+    
+    securitiy_dic = {
+        1 : [[0], [1], [2], [3]],
+        2 : [[0, 1], [2, 3]],
+        3 : [[0, 3], [1, 2], [0, 2], [1, 3]],
+        4 : [[0, 1, 2], [1, 2, 3], [2, 0, 3], [0, 1, 3]],
+        5 : [[0, 1, 2, 3]]
+        }
 
+    camera_dic = {}
+    for i in camera:
+        camera_lst = []
+        # 하 상 우 좌 
+        for lrud in [[-1, 0], [1, 0], [0, 1], [0, -1]]:
+            k = 0
+            tmp = set()
+            # i
+            while 0 <= i[0] + k * lrud[0] < N and 0 <= i[1] + k * lrud[1] < M and\
+                room[i[0] + k * lrud[0]][i[1] + k * lrud[1]] != 6:
+                    tmp.add((i[0] + k * lrud[0], i[1] + k * lrud[1]))
+                    k += 1
+            # print(tmp)
+            camera_lst += [tmp]
+        # print(camera_lst)
+        for coverage in securitiy_dic[room[i[0]][i[1]]]:
+            total_set = set()
+            for specific in coverage:
+                total_set = total_set | camera_lst[specific]
+            camera_dic[i] = camera_dic.get(i, []) + [total_set]
         
+    # print(camera_dic)
+    max_val = 0
+    # print(camera_dic)
+    def search(idx, sett):
+        global max_val 
+        # print(sett)
+        # print(idx)
+        if idx == len(camera) - 1:
+            for i in camera_dic[camera[idx]]:
+                if len(sett | i) > max_val:
+                    max_val = len(sett | i)
+            return
+                
+        # print(camera[idx])
+        for i in camera_dic[camera[idx]]:
+            search(idx + 1, sett | i)
 
+    search(0, set())
+    print(mamamax - max_val)
+            
+        
+        
+        
+    # 카메라의 위치별로 
 
             
+
+
+                
