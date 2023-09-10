aa64d0fcbb2c76d7b3d0d21a04f251a7db2e86b7
sangbumlikeagod <oironese@naver.com>
2023-09-10 17:32:23 +0900

    9월 10일 제출

--- Gold/16637.py
+++ Gold/16637.py
@@ -18,27 +18,34 @@ dic = {
 
 
 def tupyo(nex):
-    for i in nex:
 
-        di_stack = []
-        non_digit = []
+    di_stack = []
+    non_digit = []
+    idx = 0
+    while idx < len(nex):
         # print(di_stack)
         # print(non_digit)
-        if i.isdigit():
-            di_stack.append(int(i))
+        if nex[idx] == '-' and (idx == 0 or not nex[idx - 1].isdigit()):
+                di_stack.append(int(nex[idx: idx + 2]))
+                idx += 1
+        elif nex[idx].isdigit():
+            di_stack.append(int(nex[idx]))
         else:
-            while non_digit and dic[i] > dic[non_digit[-1]]:
+            while non_digit and dic[nex[idx]] >= dic[non_digit[-1]]:
                 k = non_digit.pop()
                 a, b = di_stack.pop(), di_stack.pop()
                 if k == '+':
-                    di_stack.append(a + b)
+                    di_stack.append(a + b) 
                 elif k == '-':
                     di_stack.append(b - a)
                 elif k == '*':
                     di_stack.append(a * b)
-            non_digit.append(i)
+            non_digit.append(nex[idx])
+        idx += 1
             
     while non_digit:
+        print(di_stack)
+        print(non_digit)
         k = non_digit.pop()
         a, b = di_stack.pop(), di_stack.pop()
         if k == '+':
@@ -54,15 +61,40 @@ def tupyo(nex):
 
 dodo = []
 for i in range(len(nex)):
-    print(nex[i])
     if nex[i] == '+' or nex[i] == '-':
         dodo.append(i)
 print(dodo)
-            
-            
-        
-    
 
+# 부분집합에서 어떤 조건 하나를 추가할 것 
+
+dodo_clone = [0] * len(dodo)
+max_val = -float('inf')
+def conditional_subset(lst, idx):
+    global max_val
+    if idx == len(lst):
+        st = nex
+        for i in range(len(lst) - 1, -1 , -1):
+            
+            if lst[i] and st[dodo[i]] == '+':
+                st = st[:dodo[i] - 1] + str(int(st[dodo[i] - 1]) + int(st[dodo[i] + 1]))      + st[dodo[i] + 2:]
+            elif   lst[i] and st[dodo[i]] == '-':
+                st = st[:dodo[i] - 1] + str(int(st[dodo[i] - 1]) - int(st[dodo[i] + 1]))      + st[dodo[i] + 2:]
+        print(st)
+
+        var = tupyo(st)
+        print(var)
+        if var > max_val:
+            max_val = var
+        return 
+    conditional_subset(lst, idx + 1)
+    if idx == 0 or abs(dodo[idx] - dodo[idx - 1]) != 2 or not lst[idx - 1]:
+        lst[idx] = 1
+        conditional_subset(lst, idx + 1)
+        lst[idx] = 0
+       
+conditional_subset(dodo_clone, 0)
+
+print(max_val)
 # 후진 계산법을 활용해야하는데 ? 
 # 후입가산기였나.
 # 단순 부분집합문제가 아님 
