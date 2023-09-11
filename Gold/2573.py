36db7c398d6d1cdaab17f9d04d3423c98aa21569
sangbumlikeagod <oironese@naver.com>
2023-09-12 08:38:02 +0900

    9월 12일 제출

--- Gold/2573.py
+++ Gold/2573.py
@@ -9,21 +9,81 @@ M, N = map(int, input().split())
 mass = M * N
 from collections import deque
 que = deque()
-lst = []
+sea = []
 for i in range(M):
     lst = list(map(int, input().split()))
-    for j in lst:
-        if j:
+    for j in range(len(lst)):
+        if lst[j]:
             mass -= 1
             que.append((i, j))
-
-print(que)
+    sea += [lst]
+# print(*sea, sep = '\n', end='\n')
+# print()
+# print(que)
 
 one_dung = True
-
+scount = 0
 while one_dung:
+    change = []
     for _ in range(len(que)):
         i, j = que.popleft()
+        
         tmp = []
+        ttmp = sea[i][j] 
         for nwes in ((1, 0), (0, 1), (-1, 0), (0, -1)):
-            if i + nwes[0]
+            if 0 <= i + nwes[0] < M and 0 <= j + nwes[1] < N:
+                if sea[i + nwes[0]][j + nwes[1]] == 0 and ttmp:
+                    ttmp -= 1
+                else:
+                    tmp.append((i + nwes[0], j + nwes[1]))
+        # 흐음 어딨냐 
+        
+        if ttmp:
+            que.append((i, j))
+        change.append((i, j, ttmp))
+        
+    for arg in change:
+        x, y, val = arg
+        sea[x][y] = val
+    
+    
+    # print(que)
+    scount  += 1
+    if que:
+        new_queue = deque()
+        new_queue.append(tuple(que[0]))
+        tmp_count = 0
+        visited = [[0] * N for _ in range(M)]
+        # print(new_queue[0])
+        visited[new_queue[0][0]][new_queue[0][1]] = 1
+        while new_queue:
+  
+            tmp_count += 1
+            x, y = new_queue.popleft()
+            for nwes in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+                if 0 <= x + nwes[0] < M and 0 <= y + nwes[1] < N\
+                    and not visited[x + nwes[0]][y + nwes[1]]\
+                    and sea[x + nwes[0]][y + nwes[1]]:
+                    visited[x + nwes[0]][y + nwes[1]] = 1
+                    new_queue.append(( x + nwes[0] , y + nwes[1] ))    
+        
+    else:
+        # print(count)
+        one_dung = False
+        
+    if tmp_count < len(que):
+        # print(count)
+        one_dung = False
+
+        
+for i in range(M):
+    if sea[i].count(0) != N:
+        print(scount)
+        break
+else:
+    print(0)
+    
+    
+    # print(*sea, sep = '\n', end='\n')
+    # print()
+                
--- Gold/2632.py
+++ Gold/2632.py
@@ -4,6 +4,71 @@ sys.stdin = open(__file__.split('\\')[-1][:-3] + '.txt','r')
 st = sys.stdin.readline
 C = int(st())
 A, B = map(int, st().split())
-lst_A = 
+pizza_A = []
+pizza_B = []
 for _ in range(A):
+    pizza_A += [int(st())]
     
+for _ in range(B):
+    pizza_B += [int(st())]
+    
+# print(pizza_A, pizza_B)
+dic = {}
+count = 0
+def pizza_A_find():
+    global count
+    ss = sum(pizza_A)
+    if ss <= C:
+        count += pizza_B_find(C - ss)
+
+        
+    for i in range(A):
+        # print(i, end=' ')
+        local = pizza_A[i]
+        two_pointer = (i + 1) % A
+        while local < C:
+            if local == ss:
+                break
+            if two_pointer == i:
+                break
+            # print(two_pointer, end=' ')
+            if C - local in dic:
+                count += dic[C - local]
+            else:
+                dic[C- local] = pizza_B_find(C - local)   
+                count += dic[C - local]   
+            local += pizza_A[two_pointer]
+            two_pointer = (two_pointer + 1) % A
+        if local == C:
+            count += 1
+
+            
+def pizza_B_find(num):
+
+    if num == 0:
+        return 1
+    elif sum(pizza_B) == num:
+        return 1
+    for i in range(B):
+        local = pizza_B[i]
+        two_pointer = (i + 1) % A
+        while local < num:
+            if two_pointer == i:
+                break
+            local += pizza_A[two_pointer]
+            two_pointer = (two_pointer + 1) % A
+        if local == num:
+            return 1
+    return 0
+pizza_A_find()
+
+# print(dic)
+print(count)
+            
+
+
+# 1000개가 있을 때 확인해야하는 개수가 
+# 1000 + 999 + 998 ~~~ 1001 * 500 인걸로 예상됨 그걸 총 두번 반복할 수 있음
+# 그 이외에는 줄이는 방법에 대해서 생각해야함 아니지
+# 빠지는게 1000개 짜리 가 가능하니까 가능성은 또 무궁무진함 
+# 메모이 제이션으로 같은 값은 어느정도 방어하는 방법을 만들 수도 있음 
