4b3df28d54b1f1f9b55e534bde893da0d644ed7d
김상범 <oironese@naver.com>
2023-09-15 17:58:21 +0900

    이거 왜 안풀려???

--- PLATINUM/20188-4.py
+++ PLATINUM/20188-4.py
@@ -1,29 +1,68 @@
 import sys
 sys.stdin = open(__file__.split('\\')[-1][:-5] + '.txt','r')
 st = sys.stdin.readline
-
+sys.setrecursionlimit(400000)
 N = int(st())
 dic = {}
 for i in range(N - 1):
     a, b = map(int, st().split())
     dic[a] = dic.get(a, []) + [b]
     dic[b] = dic.get(b, []) + [a]
-print(dic)
+
 
 visited = [0] * (N + 1)
-def dfs(node, layer, forbrother): 
+count = 0
+
+
+def dfs(node, layer, valueofbro, numofbro): 
+    global count
+    # print(node, layer, valueofbro, numofbro)
     visited[node] = 1
-    print(node, layer, forbrother)
-    brother = 0
-    for_return = 0
-    for_this_brother = 0
+    base_value = valueofbro 
+    base_brother_num = numofbro
+    count += base_value + base_brother_num
+    # print(f'{node} \t {base_value + base_brother_num} 를 더함')
+    layer_val = 0
+    layer_bro = 0
+
     for i in dic[node]:
         if not visited[i]:
-            forparent, forb = dfs(i, layer + 1, forbrother + for_this_brother)
-            for_this_brother += forb
-            for_return += forparent + layer + 1
-            brother += 1
-    return (for_return, brother)
+            if len(dic[i]) == 1: 
+                # print('여긴 지나감')
+                count += layer + 1 + base_value + base_brother_num + layer_val + base_brother_num + layer_bro
+                layer_bro += 1
+                layer_val += layer + 1
+            else:
+                forparent, forb = dfs(i, layer + 1, base_value + base_brother_num+ layer_val, base_brother_num + layer_bro)
+                layer_bro += forb
+                count += forparent
+                layer_val += forparent
+    return (layer_val + layer, layer_bro + 1)
 
-print(dfs(1, 0, 0))
+def dfs(node, layer, valueofbro, numofbro): 
+    global count
+    # print(node, layer, valueofbro, numofbro)
+    visited[node] = 1
+    base_value = valueofbro 
+    base_brother_num = numofbro
+    count += base_value + base_brother_num
+    # print(f'{node} \t {base_value + base_brother_num} 를 더함')
+    layer_val = 0
+    layer_bro = 0
+
+    for i in dic[node]:
+        if not visited[i]:
+            if len(dic[i]) == 1: 
+                # print('여긴 지나감')
+                count += layer + 1 + base_value + base_brother_num + layer_val + base_brother_num + layer_bro
+                layer_bro += 1
+                layer_val += layer + 1
+            else:
+                forparent, forb = dfs(i, layer + 1, base_value + base_brother_num+ layer_val, base_brother_num + layer_bro)
+                layer_bro += forb
+                count += forparent
+                layer_val += forparent
+    return (layer_val, layer_bro + 1)
+dfs(1, 0, 0, 0)
+print(count)
 
