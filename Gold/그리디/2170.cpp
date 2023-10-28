bb1dd98d4b746de4dbd0a9f340d581bfb030418a
sangbumlikeagod <oironese@naver.com>
2023-10-28 20:38:31 +0900

    제출

--- Gold/그리디/2170.cpp
+++ Gold/그리디/2170.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <fstream>
+#include <queue>
+#include <vector>
+using namespace std;
+
 
 int main(){
+    ifstream cin("2170.txt", ios_base::in);
     int N;
-    
+    cin >> N;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    ios::sync_with_stdio(false), cin.tie(0), cin.tie(0);
+
+    for (int li; li < N; li++){
+        int S, E;
+        cin >> S >> E;
+        pq.push(make_pair(S, E));
+    }
+    int end = pq.top().first - 1;
+    int total = 0;
+    while(pq.empty() == false){
+        cout << pq.top().first << ' ' << pq.top().second << '\n';
+        if (pq.top().first > end){
+            total += pq.top().second - pq.top().first;
+            end = pq.top().second;
+        } else if (pq.top().second > end) {
+            total += pq.top().second - end;
+            end = pq.top().second;
+        } 
+        pq.pop();
+    }
+    cout << total;
+
 }
--- Gold/이진탐색트리/5639.cpp
+++ Gold/이진탐색트리/5639.cpp
@@ -13,11 +13,11 @@ struct node{
         if (arg -> val >= val){
             if (rightchild == nullptr){
                 rightchild = arg;
-                cout << this->val << "뿌잉" << this->rightchild << '\n';
+                // cout << this->val << "뿌잉" << this->rightchild << '\n';
                 (*arg).parent = this;
             } else {
                 rightchild -> insert(arg);
-                cout << this->val << "뿌잉" << '\n';
+                // cout << this->val << "뿌잉" << '\n';
                 (*arg).parent = this;
 
                 // (*arg->parent) = *this;
@@ -33,17 +33,40 @@ struct node{
 
 };
 
+void dfs(node* nd){
+    if (nd->leftchild != nullptr){
+        dfs(nd->leftchild);
+    }
+    if (nd->rightchild != nullptr){
+        dfs(nd->rightchild);
+    }
+    cout << nd->val << '\n'; 
+}
+
 int main(){
-    node a = {1};
-    cout << a.val << '\n';
-    node b = {3};
-    node c = {2};
-    a.insert(&b);
-    a.insert(&c);
-    node* root = &a;
-    cout << a.rightchild->val << '\n';
-    cout << a.rightchild->leftchild->val << '\n';
-    cout << a.size << ' ' << a.rightchild -> size << ' ' << a.rightchild -> leftchild -> size;
-    cout << a.rightchild -> parent -> val;
-    
+    node *root = nullptr;
+    ifstream cin("5639.txt", ios_base::in);
+    ios::sync_with_stdio(false), cin.tie(0), cin.tie(0);
+    int n;
+    while (cin >> n)
+    {
+        // cout << n << '\n';
+        node *a = new node{n};
+
+        if (root == nullptr){
+            // cout << '\t' << n << '\n';
+            root = a;
+        } else {
+            // cout << '\t' << n << '\n';
+            (*root).insert(a);
+        }
+    }
+    // cout << root -> val;
+    // cout << a.val << '\n';
+
+    // cout << a.rightchild->val << '\n';
+    // cout << a.rightchild->leftchild->val << '\n';
+    // cout << a.size << ' ' << a.rightchild -> size << ' ' << a.rightchild -> leftchild -> size;
+    // cout << a.rightchild -> parent -> val;
+    dfs(root);
 }
