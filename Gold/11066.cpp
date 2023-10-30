5f5e7c4a185c815e547100baaa4d9d2fa9d7bd96
sangbumlikeagod <oironese@naver.com>
2023-10-30 17:52:10 +0900

    제출

---
+++ Gold/11066.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int ptsum(int s, int e, ){
+    if (e - s == 1){
+        return 
+    }
+
+}
+
+
+int main(){
+    ifstream cin("11066.txt", ios_base::in);
+    ios::sync_with_stdio(false), cin.tie(0), cin.tie(0);
+    int T;
+    cin >> T;
+    // cout << T;
+    for (int i = 0; i < T; i++){
+        int* arr[501][501];
+        // cout << arr[0][0];
+
+        int total = 0;
+
+        cout << total << '\n';
+    }
+
+
+}   
