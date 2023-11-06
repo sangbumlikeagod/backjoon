5ffdcd6f2081c62ac69540470b6f40324db96b9c
sangbumlikeagod <oironese@naver.com>
2023-11-06 17:57:31 +0900

    1106 submit

--- Gold/BFS/13549.cpp
+++ Gold/BFS/13549.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 using namespace std;
 
-vector<int> visited(200000, 0);
+vector<int> visited(200000, 100001);
 
 int main(){
     ifstream cin("13549.txt", ios_base::in);
@@ -14,23 +14,33 @@ int main(){
     int s;
     int e;
     cin >> s >> e;
-    visited[s] = 1;
+    visited[s] = 0;
     int round;
     int location;
-
     que.push(make_pair(0, s));
     while (!que.empty())
     {
-        round, location = que.top().first, que.top().second;
-        if (location > 0){
-            if (location * 2 < 200000 && !visited[location * 2]){
-                visited
-            }
-        }
-
+        round = que.top().first;
+        location = que.top().second;
+        // cout << round <<' ' << location << '\n';  
         que.pop();
+        if (location == e) {
+            cout << round;
+            break;
+        }
+        if (location * 2 < e * 2 && round < visited[location * 2]){
+            visited[location * 2] = round;
+            que.push(make_pair(round, location * 2));
+        }
+        if (location + 1 < 200000 && round + 1 < visited[location + 1]){
+            visited[location + 1] = round + 1;
+            que.push(make_pair(round + 1, location + 1));
+        }
+        if (location - 1 >= 0 && round + 1 < visited[location - 1]){
+            visited[location - 1] = round + 1;
+            // cout << location - 1 << '\n';
+            que.push(make_pair(round + 1, location - 1));
+        }
     }
-    
-
 
 }
---
+++ Gold/STRING/9935-2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <stack>
+#include <string>
+#include <queue>
+using namespace std;
+
+vector<char> vec(1000000, ' ');
+string s1;
+string s2;
+pair<int, int>* null_pair = new pair<int, int>(-1, -1); 
+
+int main(){
+    ios::sync_with_stdio(false), cin.tie(0), cin.tie(0);
+    ifstream cin("9935.txt", ios_base::in);    
+    cin >> s1 >> s2;
+    // cout << s1.length() << '\n' << s2.length();
+    int levelS1, levelS2;
+    levelS1 = s1.length();
+    levelS2 = s2.length() - 1;
+    int idx = 0;
+    int idxW2;
+
+    while (idx < levelS1){
+        vec[idx] = s1[idx++]; 
+        idxW2 = levelS2;
+        while (idxW2 >= 0 && idx - 1 >= levelS2 && vec[idx - 1] == s2[idxW2] ) idx--; idxW2--;
+        if (idxW2 != -1){
+            idx += levelS2 - idxW2;
+        } else {
+            levelS1 -= levelS2;
+        }
+    }
+    cout << levelS1 << '\n';
+    for (int i = 0; i < levelS1; i++){
+        cout << vec[i];
+    }
+    }
