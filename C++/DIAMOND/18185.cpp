b207e2338c99ad7a6b6641cb74d0d48e13a377b6
sangbumlikeagod <oironese@naver.com>
2024-01-25 18:00:41 +0900

    commit

---
+++ C++/DIAMOND/18185.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+
+
+int main(){
+    ios::sync_with_stdio(false), cin.tie(0), cin.tie(0);
+    ifstream cin("18185.txt", ios_base::in);    
+        
+
+
+}
