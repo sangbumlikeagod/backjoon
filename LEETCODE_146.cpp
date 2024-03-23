17d93edf93282315f725a3a2555c484d3f7c5bcf
sangbumlikeagod <oironese@naver.com>
2024-03-23 20:47:28 +0900

    commit LRU Cache

--- LEETCODE_146.cpp
+++ LEETCODE_146.cpp
@@ -6,114 +6,114 @@ struct node
 {
     int key;
     int val;
-    node* before;
-    node* next;
+    node *before;
+    node *next;
     node(int key, int val) : key(key), val(val), before(nullptr), next(nullptr){};
 };
 
 class LinkedList
 {
-    public:
+public:
     int metaTotalNode;
     int metaMAXNUM;
-    node* head;
-    node* tail;
+    node *head;
+    node *tail;
     void popHead()
     {
-        if (head->next)
-        {
-            head->next->before = nullptr;
-        }
-        
-        head = head->next;
-        
+
+        head->next->next->before = head;
+        head->next = head->next->next;
+
         metaTotalNode--;
     }
-    void pushTail(node* node)
+    void pushTail(node *node)
     {
-        if (head == nullptr)
-        {
-            head = node;
-            tail = node;
-            metaTotalNode++;
-            return;
-        }
-        if (tail)
-        {
-            tail->next = node; 
-        }
-        node->before = tail;
+        tail->before->next = node;
+        node->before = tail->before;
+        node->next = tail;
+        tail->before = node;
         metaTotalNode++;
     }
 
-    LinkedList() : metaMAXNUM(0) , metaTotalNode(0), head(nullptr), tail(nullptr) {}; 
-    LinkedList(int maxC) : metaMAXNUM(maxC) , metaTotalNode(0), head(nullptr), tail(nullptr) {}; 
+    LinkedList() : metaMAXNUM(0), metaTotalNode(0), head(nullptr), tail(nullptr)
+    {
+        head = new node(-1, -1);
+        tail = new node(300001, 300001);
+        head->next = tail;
+        tail->before = head;
+    };
+    LinkedList(int maxC) : metaMAXNUM(maxC), metaTotalNode(0), head(nullptr), tail(nullptr)
+    {
+        head = new node(-1, -1);
+        tail = new node(300001, 300001);
+        head->next = tail;
+        tail->before = head;
+    };
 };
 
+class LRUCache
+{
+    LinkedList list;
+    unordered_map<int, node *> cacheMapping;
 
-class LRUCache {
-     LinkedList list;
-     unordered_map<int, node*> cacheMapping;
-     
 public:
-    LRUCache(int capacity) {
-        list.metaMAXNUM = capacity; 
+    LRUCache(int capacity)
+    {
+        list.metaMAXNUM = capacity;
     }
-    
-    int get(int key) {
+
+    int get(int key)
+    {
         // 없으면
         if (cacheMapping.find(key) == cacheMapping.end())
         {
             return -1;
         }
         else
-        {   
+        {
             // 있으면
             int ans = cacheMapping[key]->val;
+            node *tmpNode = cacheMapping[key];
+            // cout << "자 이제부터 여기서 문제있는 놈을 찾아내겠어'\n";
+            tmpNode->before->next = tmpNode->next;
+            // cout << "\t" << tmpNode->before->key << "이건 반드시 -1이어야함" << '\n';
+            // cout << "\t\t" << tmpNode->next->key << "얘도 뭐가 나와야해\n";
+            tmpNode->next->before = tmpNode->before;
+            tmpNode->next = nullptr;
+            tmpNode->before = nullptr;
             list.metaTotalNode--;
-            if (cacheMapping[key]->before)
-            {
-                cacheMapping[key]->before->next =  cacheMapping[key]->next;
-            }
-            if (cacheMapping[key]->next)
-            {
-                cacheMapping[key]->next->before = cacheMapping[key]->before;
-            }
-            if (list.head == cacheMapping[key]) 
-            {
-                list.head = list.head->next;
-            }
-            if (list.tail == cacheMapping[key])
-            {
-                list.tail = list.tail->before;
-            }
-
-            list.pushTail(cacheMapping[key]);
+            // cout << "\t\t\t 이제 바뀐점이 보여야함" << list.head -> next -> key << '\n';
+            list.pushTail(tmpNode);
             return ans;
         }
     }
-    
-    void put(int key, int value) {
+
+    void put(int key, int value)
+    {
+        cout << list.metaTotalNode << '\n';
         if (cacheMapping.find(key) == cacheMapping.end())
         {
+            // 없다면
             cacheMapping[key] = new node(key, value);
-            if (list.metaMAXNUM == list.metaTotalNode)
+            if (list.metaTotalNode == list.metaMAXNUM)
             {
-                cacheMapping.erase(list.head->key);
+                int willBeErased = list.head->next->key;
+                // cout << "꽉 찼 다 " << willBeErased << '\n';
                 list.popHead();
+                cacheMapping.erase(willBeErased);
             }
             list.pushTail(cacheMapping[key]);
-        } 
-        else 
+        }
+        else
         {
             cacheMapping[key]->val = value;
+            node *tmpNode = cacheMapping[key];
+            tmpNode->before->next = tmpNode->next;
+            tmpNode->next->before = tmpNode->before;
+            tmpNode->next = nullptr;
+            tmpNode->before = nullptr;
+            list.metaTotalNode--;
+            list.pushTail(tmpNode);
         }
     }
 };
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
