c5a4be3762efbe436b57e0a3005f8606ef3c40d8
sangbumlikeagod <oironese@naver.com>
2024-03-26 23:52:09 +0900

    ææcommits

---
+++ C++/PLATINUM/DP/6989.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false), cin.tie(0), cin.tie(0);
+    ifstream cin("6989.txt", ios_base::in);
+}
