62c054b99c1da187c51315c1ab7b67fd970ac413
김상범 <oironese@naver.com>
2024-03-27 18:00:47 +0900

    ccmmcc

---
+++ C++/PLATINUM/DP/6989.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+
+
+int main(){
+    ios::sync_with_stdio(false), cin.tie(0);
+    ifstream cin("6989.txt", ios_base::in);
+    
+    
+}
