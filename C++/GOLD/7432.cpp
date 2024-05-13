867287e186c7798bf3c428391f1267a4b8f4719f
sangbumlikeagod <oironese@naver.com>
2024-05-13 23:52:50 +0900

    240513: 제출

---
+++ C++/GOLD/7432.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false), cin.tie(0), cin.tie(0);
+    ifstream cin("7432.txt", ios_base::in);
+}
